0e955a75414a699d9fb3a49cf808ba570989b16d
Caso <Caso@LAPTOP-9A857FO0.lan>
2020-03-27 16:22:36 +0100

    Svolgimento punti 3,4,5,6. Videolezione (parte due)

--- src/main/java/it/polito/tdp/libretto/model/Libretto.java
+++ src/main/java/it/polito/tdp/libretto/model/Libretto.java
@@ -17,10 +17,19 @@ public class Libretto {
 	 * Aggiunge un nuovo voto al libretto
 	 * 
 	 * @param v voto da aggiungere
+	 * @return {@code true} se ha inserito il voto, 
+	 * {@code false} se non l'ha inseito perchè è in conflitto o duplicato
 	 */
 	
-	public void Add(Voto v) {
-		this.voti.add(v);
+	public boolean Add(Voto v) {
+		if(this.isCoonflittoVoto(v) || this.isDuplicato(v)) {
+			//non inserire il voto
+			return false;
+		} else {
+			// inserisci il voto perchè non è in conflitto nè duplicato
+			this.voti.add(v);
+			return true;
+		}
 	}
 	/**
 	 * dato un libretto restituisce una stringa nella quale
@@ -62,5 +71,60 @@ public class Libretto {
 		return s;
 	}
 	
+	/**
+	 * Dato il nome di un corso, ricerca se quell'esame esiste nel libretto
+	 * in caso affermativo restituisce l'oggetto {@link Voto} corrispondente
+	 * Se l'esame non esiste restituisce {@ null}
+	 * 
+	 * @param nomeCorso nome esame da cercare
+	 * @return {@link Voto} corrispondente oppure {@code null} (se non esiste)
+	 */
 	
+	public Voto cercaNomeCorso(String nomeCorso) {
+/*		for (Voto v : this.voti) {
+			if(nomeCorso.equals(v.getCorso()))
+				return v;
+		}
+		return null;
+*/		
+		int pos = this.voti.indexOf(new Voto(nomeCorso, 0, null));
+		if(pos != -1) {
+			return this.voti.get(pos);
+		} else 
+			return null;
+	}
+	
+	/**
+	 * Ritorna {@code true} se il corso specificato da {@code v}
+	 * esiste nel libretto con la stessa valutazione.
+	 * Se non esiste o se la valutazione è diversa ritorna {@code false}. 
+	 * @param v il {@link Voto} da ricercare
+	 * @return l'esistenza di un duplicato
+	 */
+	
+	public boolean isDuplicato (Voto v) {
+		Voto esiste = this.cercaNomeCorso(v.getCorso());
+		if(esiste == null)
+			return false;
+		
+//		if(esiste.getVoto() == v.getVoto())
+//			return true;
+//		else 
+			
+		return (esiste.getVoto() == v.getVoto());
+	}
+	
+	/**
+	 * Determina se esiste un voto con lo stesso nome corso ma valutazione
+	 * diversa  
+	 * @param v
+	 * @return
+	 */
+	public boolean isCoonflittoVoto (Voto v) {
+		Voto esiste = this.cercaNomeCorso(v.getCorso());
+		if(esiste == null)
+			return false;
+		
+		return (esiste.getVoto() != v.getVoto());
+	}
 }
--- src/main/java/it/polito/tdp/libretto/model/TestLibretto.java
+++ src/main/java/it/polito/tdp/libretto/model/TestLibretto.java
@@ -8,18 +8,34 @@ public class TestLibretto {
 	private void run() {
 		this.lib = new Libretto(); //crea libretto vuoto
 		
+		// 1. Inserire alcuni voti
 		Voto v1 = new Voto("Tecniche di Programmazione", 30, LocalDate.of(2020, 06, 15));
 		Voto v2 = new Voto("Analisi II", 28, LocalDate.of(2020, 06, 28));
 		
 		lib.Add(v1);
 		lib.Add(v2);
-		lib.Add(new Voto("Economia", 24, LocalDate.of(2020, 02, 14)));
+		if (lib.Add(new Voto("Economia", 24, LocalDate.of(2020, 02, 14))) == false) 
+				System.out.println("Errore  nell'inserimento di Economia");
 		
 		System.out.println(this.lib);
 		
+		// 2. Stampa tutti i voti uguali a 28
 		System.out.println(this.lib.stampaVotiUguali(28));
 		
 		System.out.println(this.lib.estraiVotiUguali(28));
+		
+		//3. Cerca un esame superato, conoscendo il nome del corso
+		String nomeCorso = "Analisi II";
+		Voto votoMancante = lib.cercaNomeCorso("Fisica I"); 
+		System.out.println("Il voto di Fisica I è "+votoMancante);
+		
+		//4. 5. Verificare voti duplicati o voti in conflitto
+		Voto economia2 = new Voto("Economia", 24, LocalDate.now());
+		Voto economia3 = new Voto("Economia", 21, LocalDate.now());
+		System.out.println("Economia con 24 è duplicato: "+lib.isDuplicato(economia2)+ "/ conflitto: "+lib.isCoonflittoVoto(economia2));
+		System.out.println("Economia con 21 è duplicato: "+lib.isDuplicato(economia3)+ "/ conflitto: "+lib.isCoonflittoVoto(economia3));
+		
+		
 	}
 	
 	public static void main(String[] args) {
